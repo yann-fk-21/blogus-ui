e0d8d42c21dad811493c99a611ccca916cbee2c5
yann-fk-21 <yann.fonkoue@facsciences-uy1.cm>
2024-08-01 18:02:24 +0100

    style(main page): re-styling card

--- components/specific/gallery.tsx
+++ components/specific/gallery.tsx
@@ -3,7 +3,7 @@ import Card from './card';
 
 export default function Gallery() {
   return (
-    <div className="grid grid-cols-4 py-10 gap-8 px-20">
+    <div className="grid grid-cols-4 py-5 gap-8 px-20">
       <Card />
       <Card />
       <Card />
